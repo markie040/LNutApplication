dc2ea54b7cc71e753ba028c30c39bb51c97d3454
smitm040 <mark@languagenut.com>
2013-10-15 10:56:58 +0100

    Databse Structure

---
+++ js/database.js
@@ -0,0 +1,34 @@
+	function Database()
+	{
+		
+	}
+	
+	Database.prototype = 
+	{
+		constructor: Database,
+		
+		insert: function() 
+		{
+		},
+		
+		update:function() 
+		{
+		},
+		
+		Select:function() 
+		{
+		},
+		
+		Delete:function() 
+		{
+		},
+		
+		sync:function()
+		{
+		},
+		
+		clear()
+		{
+		},
+		
+	}
---
+++ js/localstoreage.js
@@ -0,0 +1,38 @@
+	function LocalStorage()
+	{
+		
+	}
+	
+	LocalStorage.prototype = 
+	{
+		constructor: LocalStorage,
+		
+		store: function() 
+		{
+		},
+		
+		updateStore:function() 
+		{
+		},
+		
+		getstore:function() 
+		{
+		},
+		
+		removeStore:function() 
+		{
+		},
+		
+		syncStore:function()
+		{
+		},
+		
+		completeSyncStore()
+		{
+		},
+		
+		completeClearStore()
+		{
+		},
+		
+	}
